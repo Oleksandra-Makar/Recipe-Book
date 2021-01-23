e35a6b4a284e60b3f107400ec9f06a8ad0304e3e
Олександра Макар <oleksandra_makar@epam.com>
2021-01-23 17:15:09 +0200

    Added navigarion; Now user can Add ingredients to the list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model'
 
 @Component({
@@ -7,16 +7,21 @@ import { Recipe } from '../recipe.model'
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test',
     'https://www.cookingclassy.com/wp-content/uploads/2019/09/meatballs-21.jpg'),
-    new Recipe('A Test Recipe', 'This is simply a test',
+    new Recipe('Another Test Recipe', 'This is simply a test',
     'https://www.cookingclassy.com/wp-content/uploads/2019/09/meatballs-21.jpg')
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
